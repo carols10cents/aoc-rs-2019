030b1ac85cb3bd4ad668d9fb3d1f7eec47690c38
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-01 20:19:18 -0500

    Add examples as failing tests with just enough code to compile

--- aoc-01-01/src/main.rs
+++ aoc-01-01/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn fuel(mass: i32) -> i32 {
+    mass
+}
+
+#[cfg(test)]
+mod tests {
+    use super::fuel;
+
+    #[test]
+    fn example_test_cases() {
+        assert_eq!(fuel(12), 2);
+        assert_eq!(fuel(14), 2);
+        assert_eq!(fuel(1969), 654);
+        assert_eq!(fuel(100756), 33583);
+    }
+}
