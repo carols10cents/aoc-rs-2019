ed1a108feba7e3f24de11139f846d697bf213396
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-01 20:47:00 -0500

    Add test cases for new requirements to include fuel for fuel
    Not sure I like the name for this function. Just enough code to get the
    tests to compile but not pass.
    
    Also feeling pleased about my choice for i32 as the types used rather
    than u32 :P

--- aoc-01-02/src/main.rs
+++ aoc-01-02/src/main.rs
@@ -20,9 +20,13 @@ fn fuel(mass: i32) -> i32 {
     mass / 3 - 2
 }
 
+fn fuel_for_mass_and_fuel(mass: i32) -> i32 {
+    fuel(mass)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::fuel;
+    use super::*;
 
     #[test]
     fn example_test_cases() {
@@ -30,5 +34,12 @@ mod tests {
         assert_eq!(fuel(14), 2);
         assert_eq!(fuel(1969), 654);
         assert_eq!(fuel(100756), 33583);
+    }
+
+    #[test]
+    fn example_fuel_for_mass_and_fuel_test_cases() {
+        assert_eq!(fuel_for_mass_and_fuel(14), 2);
+        assert_eq!(fuel_for_mass_and_fuel(1969), 966);
+        assert_eq!(fuel_for_mass_and_fuel(100756), 50346);
     }
 }
