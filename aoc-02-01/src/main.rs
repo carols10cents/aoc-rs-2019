a702d09feb6e0714a8c3239c1ed9455042d58d0f
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-02 20:58:28 -0500

    Fix warning about unused variable

--- aoc-02-01/src/main.rs
+++ aoc-02-01/src/main.rs
@@ -27,6 +27,6 @@ mod tests {
     #[should_panic(expected = "Unknown opcode: 42")]
     fn unknown_opcode_panics() {
         let program = vec![42];
-        let answer = run_intcode(program);
+        run_intcode(program);
     }
 }
