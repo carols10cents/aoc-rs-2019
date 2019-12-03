315522a728fd3e84012b1b384b00ea94499d3892
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-02 20:51:21 -0500

    Add a trivial test that 99 stops and doesn't change anything

--- aoc-02-01/src/main.rs
+++ aoc-02-01/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn run_intcode(program: Vec<i32>) -> Vec<i32> {
+    program
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn opcode_99_ends() {
+        let program = vec![99];
+        let answer = run_intcode(program);
+        assert_eq!(answer, vec![99]);
+    }
+}
