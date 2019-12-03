abd45c453b548d0e308a982001c6e6ccb92c82d6
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-02 20:55:41 -0500

    Add a failing test for unknown opcode

--- aoc-02-01/src/main.rs
+++ aoc-02-01/src/main.rs
@@ -16,4 +16,11 @@ mod tests {
         let answer = run_intcode(program);
         assert_eq!(answer, vec![99]);
     }
+
+    #[test]
+    #[should_panic(expected = "Unknown opcode: 42")]
+    fn unknown_opcode_panics() {
+        let program = vec![42];
+        let answer = run_intcode(program);
+    }
 }
