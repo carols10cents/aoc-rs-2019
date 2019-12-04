76646344e359b7721d0b2baeeca1636d1780c639
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-03 20:35:00 -0500

    Sketch some thoughts and plans

--- aoc-03-01/src/main.rs
+++ aoc-03-01/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// ## Assumptions/thoughts
+//
+// - The central port is at (0, 0)
+// - Right and Up amounts can be represented by positive numbers
+// - Left and Down amounts can be represented by negative numbers
+// - Right and Left are in the X direction
+// - Up and Down are in the Y direction
+//
+// ## Plan
+//
+// - Write code to parse "R2,D5" strings into a more convenient data structure
+// - Write code that can turn paths into a list of points that each wire touches
+// - Find the points that both wires have in their list of points
+// - The Manhattan distance of any point to (0, 0) is x + y
+// - Sort the intersection points by Manhattan distance and take the min
+
+enum PathPart {
+    XDirection(i32),
+    YDirection(i32),
+}
+
+fn locations_from_path(path: Vec<PathPart>) -> Vec<(i32, i32)> {
+    vec![]
+}
