802bbbcdbdb5c659b5af56f3fe72f089d931fe8d
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-04 20:30:43 -0500

    Oops forgot a semicolon

--- aoc-03-01/src/main.rs
+++ aoc-03-01/src/main.rs
@@ -178,7 +178,7 @@ mod tests {
         assert_eq!(closest, 159);
 
         let w1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
-        let w2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+        let w2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
 
         let closest = closest_crossed_wires(w1, w2);
         assert_eq!(closest, 135);
