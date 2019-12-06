cd94455579a92b24bdfa55c975707019ed1cb31b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-05 21:48:33 -0500

    Write a failing test for number to vec of digits conversion

--- aoc-04-01/src/main.rs
+++ aoc-04-01/src/main.rs
@@ -5,3 +5,19 @@ fn main() {
         // if so, see if there are any duplicates by putting in a set and seeing if len < 6
     // }
 }
+
+fn number_to_digits(num: i32) -> Vec<i32> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn convert_number_to_digits() {
+        let number = 145852;
+        let digits = number_to_digits(number);
+        assert_eq!(digits, vec![1, 4, 5, 8, 5, 2]);
+    }
+}
