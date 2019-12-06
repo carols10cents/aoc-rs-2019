0bd2764bf2ef671aac3a233ac312124e0d4a145d
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-05 21:45:53 -0500

    Sketch some ideas

--- aoc-04-01/src/main.rs
+++ aoc-04-01/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    // for candidate in 145852..=616942 {
+        // turn number into vector of digits
+        // sort digits ascending and see if it's the same after
+        // if so, see if there are any duplicates by putting in a set and seeing if len < 6
+    // }
 }
