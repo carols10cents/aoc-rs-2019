8c14e5794c2b085c31607b426bd1e55bc883dde3
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-07 20:57:54 -0500

    Implement orbit counting, I know I don't have the String/&strs right yet

--- aoc-06-01/src/main.rs
+++ aoc-06-01/src/main.rs
@@ -1,9 +1,30 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
 }
 
 fn orbit_count(input: &str) -> usize {
-    0
+    let mut orbits = HashMap::new();
+
+    for line in input.lines() {
+        let bodies = line.split(")");
+
+        let orbited = bodies.next().expect("Must have a body being orbited");
+        let orbiting = bodies.next().expect("Must have a body doing the orbiting");
+
+        orbits.insert(orbiting, orbited);
+    }
+
+    let mut total = 0;
+    for body in orbits.keys() {
+        let mut current = body;
+        while current != "COM" {
+            total += 1;
+            current = orbits.get(current).expect("Must be orbiting something");
+        }
+    }
+    total
 }
 
 #[cfg(test)]
