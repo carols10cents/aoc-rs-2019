a4103401c00495bf2bb03eb8c6dc61ab65a9517f
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-07 20:49:18 -0500

    Write a failing test for one direct orbit

--- aoc-06-01/src/main.rs
+++ aoc-06-01/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn orbit_count(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_direct_orbit_only() {
+        let input = "COM)A";
+        let total = orbit_count(input);
+        assert_eq!(total, 1);
+    }
+}
