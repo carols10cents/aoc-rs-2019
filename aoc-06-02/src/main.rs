4c12ae3fdb823f8404d6c7428150c4c6f9684c23
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-08 21:33:01 -0500

    Nope, not sure why I thought that would work. Try pattern matching refs

--- aoc-06-02/src/main.rs
+++ aoc-06-02/src/main.rs
@@ -65,7 +65,7 @@ fn num_transfers_to_santa(input: &str) -> usize {
         }
 
         for c in candidates {
-            if let Some(inner) = *orbits.get(c) {
+            if let Some(&inner) = orbits.get(c) {
                 if !visited.contains(inner) {
                     new_candidates.insert(inner);
                 }
@@ -73,7 +73,7 @@ fn num_transfers_to_santa(input: &str) -> usize {
 
             for (outer, _) in orbits
                 .iter()
-                .filter(|(k, &v)| v == c && !visited.contains(k)) {
+                .filter(|(&k, &v)| v == c && !visited.contains(k)) {
                 new_candidates.insert(outer);
             }
         }
