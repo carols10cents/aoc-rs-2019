48db3ee48a2c187a55a64f52e278f282ecaff9d9
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-08 19:44:10 -0500

    Oops need to add another stopping condition to avoid overflowing the stack

--- aoc-06-02/src/main.rs
+++ aoc-06-02/src/main.rs
@@ -66,7 +66,7 @@ fn inner_num_transfers_to_santa(
         let move_in = orbits.get(currently_orbiting);
         let inward = move_in.map(|body| inner_num_transfers_to_santa(orbits, santa_orbiting, body));
 
-        let outward = orbits.iter().filter(|(_, &v)| v == currently_orbiting).map(|(body, _)| inner_num_transfers_to_santa(orbits, santa_orbiting, body)).min();
+        let outward = orbits.iter().filter(|(_, &v)| v == currently_orbiting && v != "YOU").map(|(body, _)| inner_num_transfers_to_santa(orbits, santa_orbiting, body)).min();
 
         1 + match (inward, outward) {
             (Some(i), Some(o)) => cmp::min(i, o),
