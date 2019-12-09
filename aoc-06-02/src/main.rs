d36565469d85c9283007843cf5f730dfb926f18d
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-08 19:16:59 -0500

    Revert "Try storing both out and in in the HashMap"
    This reverts commit 0e1c2fb30653939084ee391a08c6707a4665ff8b.

--- aoc-06-02/src/main.rs
+++ aoc-06-02/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let input = include_str!("../input");
 }
 
-fn orbit_graph(input: &str) -> HashMap<&str, Vec<&str>> {
+fn orbit_graph(input: &str) -> HashMap<&str, &str> {
     let mut orbits = HashMap::new();
 
     for line in input.lines() {
@@ -13,8 +13,7 @@ fn orbit_graph(input: &str) -> HashMap<&str, Vec<&str>> {
         let orbited = bodies.next().expect("Must have a body being orbited");
         let orbiting = bodies.next().expect("Must have a body doing the orbiting");
 
-        orbits.entry(orbiting).or_insert(Vec::new()).push(orbited);
-        orbits.entry(orbited).or_insert(Vec::new()).push(orbiting);
+        orbits.insert(orbiting, orbited);
     }
 
     orbits
