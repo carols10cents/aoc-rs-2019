5eb27b84eb13dfcda87546bcec809ac0fab7d8ad
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-10 20:16:10 -0500

    Add the other example as another assertion

--- aoc-07-02/src/main.rs
+++ aoc-07-02/src/main.rs
@@ -231,14 +231,22 @@ mod tests {
 
     #[test]
     fn run_example_in_looped_incode_computers() {
+        let phase_settings = [5, 6, 7, 8, 9];
+
         let program = vec![
             3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1,
             28, 1005, 28, 6, 99, 0, 0, 5,
         ];
-        let phase_settings = [5, 6, 7, 8, 9];
         let answer = max_signal(&program, &phase_settings);
-
         assert_eq!(answer, 139629729);
+
+        let program = vec![
+            3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+            -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+            53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10,
+        ];
+        let answer = max_signal(&program, &phase_settings);
+        assert_eq!(answer, 18216);
     }
 }
 
