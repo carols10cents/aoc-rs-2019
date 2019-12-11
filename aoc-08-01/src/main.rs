3486868829b7178292516fb5d88e2337151a6452
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-10 20:26:59 -0500

    Write a failing test for the interface I wish I had

--- aoc-08-01/src/main.rs
+++ aoc-08-01/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn parsing_data_into_images() {
+        let data = "123456789012";
+        let sample = SpaceImage::new(3, 2, data);
+
+        assert_eq!(sample.layers.len(), 2);
+        assert_eq!(sample.layers[0], vec![1, 2, 3, 4, 5, 6]);
+        assert_eq!(sample.layers[1], vec![7, 8, 9, 0, 1, 2]);
+    }
+}
