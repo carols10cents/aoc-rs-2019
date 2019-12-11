aefc056042fccd644a165a28eb296371ee7c42bb
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-10 21:19:20 -0500

    Oops, needed into_iter

--- aoc-08-02/src/main.rs
+++ aoc-08-02/src/main.rs
@@ -46,7 +46,7 @@ impl SpaceImage {
     }
 
     fn flatten(&self) -> Vec<u32> {
-        (0..self.layers[0].len()).iter().map(|i| {
+        (0..self.layers[0].len()).into_iter().map(|i| {
             self.layers.iter().map(|layer| layer[i]).skip_while(|pixel| pixel == 2).next().expect("All locations should have a color pixel in some layer")
         }).collect()
     }
