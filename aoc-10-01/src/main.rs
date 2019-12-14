4061878e79834ff48b451ddceb832bb55306f636
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-13 20:32:18 -0500

    Write up a rough plan

--- aoc-10-01/src/main.rs
+++ aoc-10-01/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// # Plan
+//
+// - For each asteroid
+//   - For each "angle" expressed by (-width..width)x(-height..height)
+//   - If you see an asteroid along that angle, stop and add 1 to the count
+//   - If you get to the end of the graph, stop
+//   - Otherwise go another interval along that angle
+// - Return asteroid with the max count
