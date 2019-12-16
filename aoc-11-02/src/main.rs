a26a2b4711825c606b07a6bc5da0392fe5dd0b66
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-15 21:15:40 -0500

    Use a better unicode character

--- aoc-11-02/src/main.rs
+++ aoc-11-02/src/main.rs
@@ -100,7 +100,7 @@ impl fmt::Display for Computer {
         for y in y_min..=y_max {
             for x in x_min..=x_max {
                 if self.white_panels.contains(&(x, y)) {
-                    write!(f, "□")?;
+                    write!(f, "█")?;
                 } else {
                     write!(f, " ")?;
                 }
