b19e4b56e0f5ca1436e86b5af0cbef4cd6c9cf25
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-15 20:16:42 -0500

    Refactor so that run_intcode is a run method on Computer

--- aoc-11-01/src/main.rs
+++ aoc-11-01/src/main.rs
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect();
 
     let simulated_stdin = Some(2);
-    let (_answer, output) = run_intcode(program, simulated_stdin);
+    let (_answer, output) = Computer::new(program, simulated_stdin).run();
     println!("{:?}", output);
 
     Ok(())
@@ -22,9 +22,22 @@ struct Computer {
     output: Vec<i64>,
     current_position: usize,
     relative_base: usize,
+    input: Option<i64>,
 }
 
 impl Computer {
+    fn new(program: Vec<i64>, input: Option<i64>) -> Computer {
+        let program: HashMap<usize, i64> = program.into_iter().enumerate().collect();
+
+        Computer {
+            program,
+            output: vec![],
+            current_position: 0,
+            relative_base: 0,
+            input,
+        }
+    }
+
     fn current_instruction(&self) -> Instruction {
         instruction(self.read_at(self.current_position))
     }
@@ -41,95 +54,86 @@ impl Computer {
     fn read_at(&self, index: usize) -> i64 {
         self.program.get(&index).copied().unwrap_or(0)
     }
-}
 
-fn run_intcode(program: Vec<i64>, input: Option<i64>) -> (HashMap<usize, i64>, Vec<i64>) {
-    let program: HashMap<usize, i64> = program.into_iter().enumerate().collect();
+    fn run(&mut self) -> (HashMap<usize, i64>, Vec<i64>) {
+        let mut current_inst = self.current_instruction();
 
-    let mut computer = Computer {
-        program,
-        output: vec![],
-        current_position: 0,
-        relative_base: 0,
-    };
-
-    let mut current_inst = computer.current_instruction();
-
-    while current_inst.opcode != 99 {
-        match current_inst.opcode {
-            1 => {
-                let input1 = computer.get_value(0);
-                let input2 = computer.get_value(1);
-                let answer = input1 + input2;
-                computer.set_value(2, answer);
-                computer.current_position += 4;
-            }
-            2 => {
-                let input1 = computer.get_value(0);
-                let input2 = computer.get_value(1);
-                let answer = input1 * input2;
-                computer.set_value(2, answer);
-                computer.current_position += 4;
-            }
-            3 => {
-                let value = input.expect("Should have had input for opcode 3");
-                computer.set_value(0, value);
-                computer.current_position += 2;
-            }
-            4 => {
-                let printing_value = computer.get_value(0);
-                computer.output.push(printing_value);
-                computer.current_position += 2;
-            }
-            5 => {
-                // jump-if-true
-                let test_value = computer.get_value(0);
-                if test_value != 0 {
-                    let jump_location = computer.get_value(1);
-                    computer.current_position = jump_location as usize;
-                } else {
-                    computer.current_position += 3;
+        while current_inst.opcode != 99 {
+            match current_inst.opcode {
+                1 => {
+                    let input1 = self.get_value(0);
+                    let input2 = self.get_value(1);
+                    let answer = input1 + input2;
+                    self.set_value(2, answer);
+                    self.current_position += 4;
                 }
-            }
-            6 => {
-                // jump-if-false
-                let test_value = computer.get_value(0);
-                if test_value == 0 {
-                    let jump_location = computer.get_value(1);
-                    computer.current_position = jump_location as usize;
-                } else {
-                    computer.current_position += 3;
+                2 => {
+                    let input1 = self.get_value(0);
+                    let input2 = self.get_value(1);
+                    let answer = input1 * input2;
+                    self.set_value(2, answer);
+                    self.current_position += 4;
                 }
+                3 => {
+                    let value = self.input.expect("Should have had input for opcode 3");
+                    self.set_value(0, value);
+                    self.current_position += 2;
+                }
+                4 => {
+                    let printing_value = self.get_value(0);
+                    self.output.push(printing_value);
+                    self.current_position += 2;
+                }
+                5 => {
+                    // jump-if-true
+                    let test_value = self.get_value(0);
+                    if test_value != 0 {
+                        let jump_location = self.get_value(1);
+                        self.current_position = jump_location as usize;
+                    } else {
+                        self.current_position += 3;
+                    }
+                }
+                6 => {
+                    // jump-if-false
+                    let test_value = self.get_value(0);
+                    if test_value == 0 {
+                        let jump_location = self.get_value(1);
+                        self.current_position = jump_location as usize;
+                    } else {
+                        self.current_position += 3;
+                    }
+                }
+                7 => {
+                    // less-than
+                    let input1 = self.get_value(0);
+                    let input2 = self.get_value(1);
+                    let answer = if input1 < input2 { 1 } else { 0 };
+                    self.set_value(2, answer);
+                    self.current_position += 4;
+                }
+                8 => {
+                    // equals
+                    let input1 = self.get_value(0);
+                    let input2 = self.get_value(1);
+                    let answer = if input1 == input2 { 1 } else { 0 };
+                    self.set_value(2, answer);
+                    self.current_position += 4;
+                }
+                9 => {
+                    // relative base adjustment
+                    let input1 = self.get_value(0);
+                    let new_rel_base = self.relative_base as i64 + input1;
+                    self.relative_base = new_rel_base as usize;
+                    self.current_position += 2;
+                }
+                other => panic!("Unknown opcode: {}", other),
             }
-            7 => {
-                // less-than
-                let input1 = computer.get_value(0);
-                let input2 = computer.get_value(1);
-                let answer = if input1 < input2 { 1 } else { 0 };
-                computer.set_value(2, answer);
-                computer.current_position += 4;
-            }
-            8 => {
-                // equals
-                let input1 = computer.get_value(0);
-                let input2 = computer.get_value(1);
-                let answer = if input1 == input2 { 1 } else { 0 };
-                computer.set_value(2, answer);
-                computer.current_position += 4;
-            }
-            9 => {
-                // relative base adjustment
-                let input1 = computer.get_value(0);
-                let new_rel_base = computer.relative_base as i64 + input1;
-                computer.relative_base = new_rel_base as usize;
-                computer.current_position += 2;
-            }
-            other => panic!("Unknown opcode: {}", other),
+            current_inst = self.current_instruction();
         }
-        current_inst = computer.current_instruction();
-    }
 
-    (computer.program, computer.output)
+        (self.program.clone(), self.output.clone())
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -231,14 +235,14 @@ mod tests {
     #[test]
     fn opcode_99_ends() {
         let program = vec![99];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&0], 99);
     }
 
     #[test]
     fn opcode_1_adds() {
         let program = vec![1, 0, 0, 0, 99];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&0], 2);
         assert_eq!(answer[&4], 99);
     }
@@ -246,21 +250,21 @@ mod tests {
     #[test]
     fn opcode_2_multiplies() {
         let program = vec![2, 3, 0, 3, 99];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&3], 6);
     }
 
     #[test]
     fn multiply_and_store_after_program() {
         let program = vec![2, 4, 4, 5, 99, 0];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&5], 9801);
     }
 
     #[test]
     fn program_keeps_going_if_an_instruction_changes() {
         let program = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&0], 30);
         assert_eq!(answer[&4], 2);
     }
@@ -268,14 +272,14 @@ mod tests {
     #[test]
     fn opcode_3_takes_input() {
         let program = vec![3, 0, 99];
-        let (answer, _output) = run_intcode(program, Some(7));
+        let (answer, _output) = Computer::new(program, Some(7)).run();
         assert_eq!(answer[&0], 7);
     }
 
     #[test]
     fn opcode_4_returns_output() {
         let program = vec![4, 2, 99];
-        let (_answer, output) = run_intcode(program, None);
+        let (_answer, output) = Computer::new(program, None).run();
         assert_eq!(output, vec![99]);
     }
 
@@ -283,12 +287,12 @@ mod tests {
     fn opcode_5_jumps_if_true() {
         // Test value is false; 42 gets printed
         let program = vec![1005, 6, 5, 104, 42, 99, 0];
-        let (_answer, output) = run_intcode(program, None);
+        let (_answer, output) = Computer::new(program, None).run();
         assert_eq!(output, vec![42]);
 
         // Test value is true; print gets jumped over
         let program = vec![1005, 6, 5, 104, 42, 99, 3];
-        let (_answer, output) = run_intcode(program, None);
+        let (_answer, output) = Computer::new(program, None).run();
         assert_eq!(output, vec![]);
     }
 
@@ -296,34 +300,34 @@ mod tests {
     fn opcode_6_jumps_if_false() {
         // Test value is false; print gets jumped over
         let program = vec![1006, 6, 5, 104, 42, 99, 0];
-        let (_answer, output) = run_intcode(program, None);
+        let (_answer, output) = Computer::new(program, None).run();
         assert_eq!(output, vec![]);
 
         // Test value is true; 42 gets printed
         let program = vec![1006, 6, 5, 104, 42, 99, 3];
-        let (_answer, output) = run_intcode(program, None);
+        let (_answer, output) = Computer::new(program, None).run();
         assert_eq!(output, vec![42]);
     }
 
     #[test]
     fn opcode_7_less_than() {
         let program = vec![1107, 4, 5, 3, 99];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&3], 1);
 
         let program = vec![1107, 5, 4, 3, 99];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&3], 0);
     }
 
     #[test]
     fn opcode_8_equals() {
         let program = vec![1108, 4, 4, 3, 99];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&3], 1);
 
         let program = vec![1108, 5, 4, 3, 99];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&3], 0);
     }
 
@@ -331,7 +335,7 @@ mod tests {
     #[should_panic(expected = "Unknown opcode: 42")]
     fn unknown_opcode_panics() {
         let program = vec![42];
-        run_intcode(program, None);
+        Computer::new(program, None).run();
     }
 
     #[test]
@@ -372,7 +376,7 @@ mod tests {
     #[test]
     fn use_parameter_modes_in_programs() {
         let program = vec![1002, 4, 3, 4, 33];
-        let (answer, _output) = run_intcode(program, None);
+        let (answer, _output) = Computer::new(program, None).run();
         assert_eq!(answer[&4], 99);
     }
 
@@ -405,17 +409,17 @@ mod tests {
     #[test]
     fn stress_tests() {
         let program = vec![1102, 34915192, 34915192, 7, 4, 7, 99, 0];
-        let (_answer, output) = run_intcode(program, None);
+        let (_answer, output) = Computer::new(program, None).run();
         assert_eq!(output, vec![1219070632396864]);
 
         let program = vec![104, 1125899906842624, 99];
-        let (_answer, output) = run_intcode(program, None);
+        let (_answer, output) = Computer::new(program, None).run();
         assert_eq!(output, vec![1125899906842624]);
 
         let program = vec![
             109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99,
         ];
-        let (_answer, output) = run_intcode(program, None);
+        let (_answer, output) = Computer::new(program, None).run();
         assert_eq!(
             output,
             vec![109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
