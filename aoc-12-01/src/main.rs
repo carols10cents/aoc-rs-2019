5da4d67199ef9435041745ad8ce4a8ded89fbf4e
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-12-17 20:38:23 -0500

    For example, suppose your scan reveals the following positions:
    Using this to represent parsing the input.

--- aoc-12-01/src/main.rs
+++ aoc-12-01/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 struct Moon {
     x: i32,
     y: i32,
@@ -24,6 +24,60 @@ impl Moon {
             dy: 0,
             dz: 0,
         }
+    }
+
+    fn parse(text: &str) -> Moon {
+        // <x=2, y=-10, z=-7>
+
+        let mut chars = text.chars().skip_while(|&c| c != '=');
+        chars.next(); // discard the =
+
+        let mut x_digits = vec![];
+        let mut current_digit = chars.next().expect("Should have been some x digits");
+        while current_digit != ',' {
+            x_digits.push(current_digit);
+            current_digit = chars
+                .next()
+                .expect("Should have been either more x digits or a comma");
+        }
+        let x_string: String = x_digits.iter().collect();
+        let x: i32 = x_string
+            .parse()
+            .expect("Should have been able to parse x_string");
+
+        let mut chars = chars.skip_while(|&c| c != '=');
+        chars.next(); // discard the =
+
+        let mut y_digits = vec![];
+        let mut current_digit = chars.next().expect("Should have been some y digits");
+        while current_digit != ',' {
+            y_digits.push(current_digit);
+            current_digit = chars
+                .next()
+                .expect("Should have been either more y digits or a comma");
+        }
+        let y_string: String = y_digits.iter().collect();
+        let y: i32 = y_string
+            .parse()
+            .expect("Should have been able to parse y_string");
+
+        let mut chars = chars.skip_while(|&c| c != '=');
+        chars.next(); // discard the =
+
+        let mut z_digits = vec![];
+        let mut current_digit = chars.next().expect("Should have been some z digits");
+        while current_digit != '>' {
+            z_digits.push(current_digit);
+            current_digit = chars
+                .next()
+                .expect("Should have been either more z digits or a >");
+        }
+        let z_string: String = z_digits.iter().collect();
+        let z: i32 = z_string
+            .parse()
+            .expect("Should have been able to parse x_string");
+
+        Moon::new(x, y, z)
     }
 
     fn apply_gravity(&mut self, moons: &[Moon]) {
@@ -61,4 +115,31 @@ fn time_step(moons: &mut [Moon], time: i32) -> i32 {
     }
 
     time + 1
+}
+
+fn parse_moons(text: &str) -> Vec<Moon> {
+    text.trim()
+        .lines()
+        .map(|moon_text| Moon::parse(moon_text))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_moons_from_text() {
+        let moons = parse_moons(
+            "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>\n",
+        );
+        let expected_moons = vec![
+            Moon::new(-1, 0, 2),
+            Moon::new(2, -10, -7),
+            Moon::new(4, -8, 8),
+            Moon::new(3, 5, -1),
+        ];
+
+        assert_eq!(moons, expected_moons);
+    }
 }
